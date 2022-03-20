7f5d9c9807d8335a680f5ff0b57f85e5f25a4670
Greg Dubicki <grzegorz.dubicki@gmail.com>
2022-03-20 09:36:16 +0000

    WIP

--- src/mypam.c
+++ src/mypam.c
@@ -4,32 +4,9 @@
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
 
-/* expected hook */
-PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
-	return PAM_SUCCESS;
-}
+PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
 
-PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-	printf("Acct mgmt\n");
-	return PAM_SUCCESS;
-}
-
-/* expected hook, this is where custom stuff happens */
-PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
-	int retval;
-
-	const char* pUsername;
-	retval = pam_get_user(pamh, &pUsername, "Username: ");
-
-	printf("Welcome %s\n", pUsername);
-
-	if (retval != PAM_SUCCESS) {
-		return retval;
-	}
-
-	if (strcmp(pUsername, "backdoor") != 0) {
-		return PAM_AUTH_ERR;
-	}
+	printf("TODO: run program for motd here. %s\n", pUsername);
 
 	return PAM_SUCCESS;
 }
